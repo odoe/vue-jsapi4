45cc05d6d928a3e3ee189f32a388163a11ca98f0
rene rubalcava <rrubalcava@esri.com>
2017-02-22 14:38:33 -0800

    simplify store

--- store/index.js
+++ store/index.js
@@ -1,22 +1,17 @@
-import Vuex from 'vuex'
 
-const store = new Vuex.Store({
-  state: {
-    map: null,
-    watchHandle: null,
-    camera: {
-      position: {
-        x: -9177356,
-        y: 4246783,
-        z: 723,
-        spatialReference: {
-          wkid: 3857
-        }
-      },
-      heading: 0,
-      tilt: 83
-    }
+export const state = {
+  map: null,
+  watchHandle: null,
+  camera: {
+    position: {
+      x: -9177356,
+      y: 4246783,
+      z: 723,
+      spatialReference: {
+        wkid: 3857
+      }
+    },
+    heading: 0,
+    tilt: 83
   }
-})
-
-export default store
+}
